4b180ddb6231d26ceda8ea47949c56e32169b7a6
fohtorme <jonasrdsantos@gmail.com>
2019-03-31 19:15:33 -0300

    Segundo desafio

--- Complexidade_Algoritmos/Desafio2/Desafio2/src/desafio2/Desafio2.java
+++ Complexidade_Algoritmos/Desafio2/Desafio2/src/desafio2/Desafio2.java
@@ -5,6 +5,8 @@
  */
 package desafio2;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 /**
@@ -12,26 +14,89 @@ import java.util.ArrayList;
  * @author jonas
  */
 public class Desafio2 {
-    
-    public ArrayList<Integer> primalNumbers[];
 
-    private static void loadPrimalNumbers(int max) {
+    private ArrayList<Integer> loadPrimalNumbers(int max) {
         ArrayList<Integer> primes = new ArrayList();
         primes.add(2);
-        for(int i=3;i<=max;i++){
-            primes.add(i);
+
+        boolean isPrime;
+
+        for (int i = 3; i <= max; i += 2) {
+
+            isPrime = true;
+
+            int sqrtI = (int) Math.round(Math.sqrt(i));
+
+            for (int j = 0; j < primes.size() && j < sqrtI; j++) {
+                if ((i % primes.get(j)) == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                primes.add(i);
+            }
         }
-        primalNumbers = primes;
+
+        return primes;
     }
-    
-    
+
+    private ArrayList<Integer> loadGoldbachSets(int min, int max, int qtd) {
+        ArrayList<Integer> primes = loadPrimalNumbers(max);
+
+        int arrLen = ((max - min) / 2) + 1;
+        int[] goldbathCount = new int[arrLen];
+
+        ArrayList<ArrayList<Integer[]>> aux = new ArrayList(arrLen);
+        
+        int val;
+
+        for (int p1 = 1; p1 < primes.size(); p1++) {
+            for (int p2 = p1; p2 < primes.size(); p2++) {
+                val = primes.get(p1) + primes.get(p2);
+                if (val >= min && val <= max ) {
+                    goldbathCount[(val - min) / 2]++;
+                }
+            }
+        }
+        
+        int index;
+        int[] greaters = new int[qtd];
+        
+        for(int i = 0; i < goldbathCount.length;i++){
+            index = 0;
+            for (int e=1; e < greaters.length; e++) {
+                if(greaters[e] < greaters[index]) index = e;
+            }
+            
+            if(goldbathCount[i]>goldbathCount[greaters[index]]){
+                greaters[index] = i;
+            }
+        }
+
+        ArrayList<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < qtd; i++) {
+            ret.add(greaters[i] * 2 + min);
+//            ret.add(goldbathCount[greaters[i]]);
+        }
+        return ret;
+    }
+
+    private void run() {
+        long inicio = System.currentTimeMillis();
+
+        System.out.println(loadGoldbachSets(1_000_000, 2_000_000, 3));
+
+        long fim = System.currentTimeMillis();
+        System.out.println(new SimpleDateFormat("ss.SSS").format(new Date(fim - inicio)));
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        loadPrimalNumbers(2000000);
-        System.out.println(primalNumbers.toString());
+        new Desafio2().run();
     }
-    
+
 }
---
+++ Complexidade_Algoritmos/Desafio2/Desafio2/src/desafio2/Desafio2v1.java
@@ -0,0 +1,129 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desafio2;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author jonas
+ */
+public class Desafio2v1 {
+
+    private ArrayList<Integer> loadPrimalNumbers(int max) {
+        ArrayList<Integer> primes = new ArrayList();
+        primes.add(2);
+
+        boolean isPrime;
+
+        for (int i = 3; i <= max; i += 2) {
+
+            isPrime = true;
+
+            int sqrtI = (int) Math.round(Math.sqrt(i));
+
+            for (int j = 0; j < primes.size() && j < sqrtI; j++) {
+                if ((i % primes.get(j)) == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime) {
+                primes.add(i);
+            }
+        }
+
+        return primes;
+    }
+
+    private int firstLesserThan(int number, ArrayList<Integer> array) {
+        // TODO: Make it faster
+        int index = -1;
+        for (int i = 0; i < array.size() && array.get(i) < number; i++) {
+            index = i;
+        }
+        return index;
+    }
+
+    private ArrayList<String> getGoldbathSets(int number, ArrayList<Integer> primalNumbers) {
+        int minIndex = 0;
+        int maxIndex = firstLesserThan(number, primalNumbers);
+        int val;
+        ArrayList<String> goldbathSets = new ArrayList();
+        while (true) {
+            if (minIndex > maxIndex) {
+                break;
+            }
+            val = primalNumbers.get(minIndex) + primalNumbers.get(maxIndex);
+            if (val == number) {
+                goldbathSets.add("("
+                        + primalNumbers.get(minIndex) + ","
+                        + primalNumbers.get(maxIndex) + ")");
+                minIndex++;
+                maxIndex--;
+                continue;
+            }
+            if (val > number) {
+                maxIndex--;
+                continue;
+            }
+            if (val < number) {
+                minIndex++;
+                continue;
+            }
+        }
+        return goldbathSets;
+    }
+
+    private void greaterGoldbathSets(int min, int max) {
+        ArrayList<Integer> primalNumbers = loadPrimalNumbers(max);
+        
+        ArrayList<String> gSets;
+
+        int[] eCount = new int[3];
+        String[] eFormated = new String[3];
+        int[] eNumber = new int[3];
+        
+        int index;
+
+        for (int g = min; g <= max; g += 2) {
+            index = 0;
+            for (int e=1; e < eCount.length; e++) {
+                if(eCount[e] < eCount[index]) index = e;
+            }
+            gSets = getGoldbathSets(g, primalNumbers);
+            if(eCount[index] < gSets.size()){
+                eCount[index]=gSets.size();
+                eFormated[index]=gSets.toString();
+                eNumber[index]=g;
+            }
+        }
+        
+        for(int i=0;i<3;i++){
+            System.out.println("G("+eNumber[i]+")="+eFormated[i]);
+        }
+    }
+
+    private void run() {
+        long inicio = System.currentTimeMillis();
+        
+        greaterGoldbathSets(1000000, 1000010);
+        
+        long fim = System.currentTimeMillis();
+        System.out.println(new SimpleDateFormat("ss.SSS").format(new Date(fim - inicio)));
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Desafio2v1().run();
+    }
+
+}
